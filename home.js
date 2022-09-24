666b19f6dcb24f596d0e2bc7013a41461a59823e
Fabrizio Fadigati <fabrizio.fadigati.dev@outlook.com>
2022-09-24 19:06:33 +0200

    New proposal - removed unnecessary css and js files

---
+++ home.js
@@ -0,0 +1,18 @@
+
+// Funzione per aggiungere una credenziale
+function addCred(){
+
+}
+
+// Funzione per modificare una credenziale
+function editCred(id){
+ 
+}
+
+// Funzione per rimuovere una credenziale
+function deleteCred(id){
+}
+
+// Funzione per esportare tutte le credenziali
+function exportCred(){
+}
