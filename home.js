4c5b09ab66a7dced83bf381f6d768f9d664d1789
Z steve <zsteveastra@gmail.com>
2022-09-27 17:40:09 +0200

    added features to logic

--- home.js
+++ home.js
@@ -4,16 +4,16 @@ function fillDefaultCred() {
     // Controlla se è già stato fatto il preset delle credenziali di default
     if (localStorage.getItem("defaultFilled") === null) {
 
-        localStorage.setItem("default-facebook", "{\"credentialName\":\"Facebook\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-instagram", "{\"credentialName\":\"Instagram\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-linkedin", "{\"credentialName\":\"LinkedIn\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-twitter", "{\"credentialName\":\"Twitter\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-gmail", "{\"credentialName\":\"Gmail\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-pinterest", "{\"credentialName\":\"Pinterest\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-github", "{\"credentialName\":\"Github\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-reddit", "{\"credentialName\":\"Reddit\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-tiktok", "{\"credentialName\":\"TikTok\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
-        localStorage.setItem("default-snapchat", "{\"credentialName\":\"Snapchat\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Facebook", "{\"credentialName\":\"Facebook\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Instagram", "{\"credentialName\":\"Instagram\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Linkedin", "{\"credentialName\":\"LinkedIn\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Twitter", "{\"credentialName\":\"Twitter\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Gmail", "{\"credentialName\":\"Gmail\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Pinterest", "{\"credentialName\":\"Pinterest\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Github", "{\"credentialName\":\"Github\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Reddit", "{\"credentialName\":\"Reddit\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Tiktok", "{\"credentialName\":\"Tiktok\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+        localStorage.setItem("default-Snapchat", "{\"credentialName\":\"Snapchat\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
 
         localStorage.setItem("defaultFilled", "true");
 
@@ -23,9 +23,11 @@ function fillDefaultCred() {
 
 function showModalPopup(credBoxId) {
 
-    // Se gli passo l'id
-    if (credBoxId !== undefined) {
 
+    // Se gli passo l'id e la credbox è una default allora ...
+    if (credBoxId !== undefined && credBoxId.includes("default-")) {
+
+        console.log("qui");
         // Recupero dati credBox
         var credentialObject = JSON.parse(localStorage.getItem(credBoxId));
 
@@ -36,11 +38,17 @@ function showModalPopup(credBoxId) {
         document.getElementById("modal-password").value = credentialObject.password;
         document.getElementById("modal-description").value = credentialObject.description;
 
+        // Rendo il credential Name delle password create in sola lettura
+        document.getElementById("modal-cred-name").readOnly = "true";
+
+        // Gestire save e remove buttons
         document.getElementById("modal-save-button").onclick = function(){saveCred(credBoxId)};
         document.getElementById("modal-remove-button").onclick = function(){deleteCred(credBoxId)};
 
     } else {
-
+        // console.log("qua");
+        // Modal Popup per creare nuova credenziale
+        document.getElementById("modal-cred-name").removeAttribute("readonly");
         document.getElementById("modal-heading").innerHTML = "ADD CREDENTIAL";
         document.getElementById("modal-save-button").onclick = function(){saveCred()};
         document.getElementById("modal-remove-button").onclick = function(){deleteCred()};
@@ -55,6 +63,9 @@ function closeModalPopup() {
 
     // Nascondi modal popup
     document.getElementById('modal-popup').style.display = 'none';
+
+    // Nascondi password se visibile
+    hidePassword();
 
     // Svuota campi modal popup
     document.getElementById("modal-cred-name").value = "";
@@ -94,13 +105,17 @@ function saveCred(credBoxId) {
 
     if (credBoxId === undefined) {
 
+        // Creazione di un random ID per la nuova credBox
         credBoxId = self.crypto.randomUUID();
 
-        // Salva credenziale nel documento
-        var cred = document.getElementById('cred-box-template').content.cloneNode(true);
+        var cred = document.getElementById('cred-box-template');
 
+        // Salva credenziale nel documento
+        document.getElementById('cred-box-template').content.cloneNode(true);
+        // Aggiungo l'id alla nuova credBox
         cred.id = credBoxId;
         // Aggiungi onclick
+        cred.addEventListener('click', showModalPopup(credBoxId));
         cred.querySelector('h3').textContent = modalCredName.value;
 
         // Aggiungi Credential Box
@@ -122,15 +137,25 @@ function saveCred(credBoxId) {
 // Funzione per rimuovere una credenziale
 function deleteCred(credBoxId) {
 
-    // Rimuovi dal documento
-    document.getElementById(credBoxId).remove();
-
-    // Rimuovi da Local Storage
-    localStorage.removeItem(credBoxId);
-
+    // fa il check se si tratta di una credenziale di default oppure no
+    if (credBoxId.includes("default-")) {
+        document.getElementById("modal-email").value = "";
+        document.getElementById("modal-password").value = "";
+        document.getElementById("modal-description").value = "";
+        localStorage.setItem("default-" + document.getElementById("modal-cred-name").value, "{\"credentialName\":\"" + document.getElementById("modal-cred-name").value + "\",\"email\":\"\",\"password\":\"\",\"description\":\"\"}");
+    } else {
+        // Rimuovi dal documento
+        document.getElementById(credBoxId).remove();
+        // Rimuovi da Local Storage
+        localStorage.removeItem(credBoxId);
+    }
+    
     closeModalPopup();
 
 }
+
+
+
 
 // Funzione per esportare tutte le credenziali
 function exportCred() {
@@ -138,3 +163,29 @@ function exportCred() {
 
 fillDefaultCred();
 
+
+
+// funzione per far vedere la password in chiaro all'utente
+function showPassword() {
+    var passwd = document.getElementById("modal-password");
+    if (passwd.type === "password") {
+        passwd.type = "text";
+    } else {
+        passwd.type = "password";
+    }
+    var eye = document.getElementById("togglePassword");
+    eye.classList.toggle('bi-eye');
+}
+
+// funzione per nascondere la password quando viene chiuso modal-popup
+function hidePassword() {
+    var passwd = document.getElementById("modal-password");
+    if (passwd.type === "text") {
+        passwd.type = "password";
+    }
+    var eye = document.getElementById("togglePassword");
+    if (eye.classList.contains('bi-eye')) {
+        eye.classList.remove('bi-eye');
+    }
+    
+}
