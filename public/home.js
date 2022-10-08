a746cb3efc89d91ad6b23181d733c83e317a208b
Fabrizio Fadigati <fabrizio.fadigati.dev@outlook.com>
2022-10-08 18:22:20 +0200

    Added Offline Service Worker, removed App Cache Manifest

--- public/home.js
+++ public/home.js
@@ -414,3 +414,15 @@ function exportCred() {
 
 // Init credenziali all'avvio dell'applicazione
 fillDefaultCred();
+
+// Registra Service Worker
+if('serviceWorker' in navigator) {
+    
+    let registration;
+  
+    const registerServiceWorker = async () => {
+      registration = await navigator.serviceWorker.register('./offline-worker.js');
+    };
+  
+    registerServiceWorker();
+}
---
+++ public/offline-worker.js
@@ -0,0 +1,39 @@
+const cacheName = 'password-manager-cache';
+
+const filesToCache = [
+    '/',
+    '/Home.html',
+    '/Home.css',
+    '/home.js',
+    '/images/add.png',
+    '/images/background.jpg',
+    '/images/export.png',
+    '/images/facebook.png',
+    '/images/favicon.png',
+    '/images/github.png',
+    '/images/gmail.png',
+    '/images/instagram.png',
+    '/images/linkedin.png',
+    '/images/lockicon.png',
+    '/images/pinterest.png',
+    '/images/reddit.png',
+    '/images/snapchat.png',
+    '/images/tiktok.png',
+    '/images/twitter.png'
+]; 
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('activate', e => self.clients.claim());
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+      caches.match(e.request)
+      .then(response => response ? response : fetch(e.request))
+    )
+  });
